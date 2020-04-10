d5b7650152cf7f0f5165f0bfffb1d270f935e488
cogu <congus8@gmail.com>
2020-04-10 21:19:19 +0200

    Enabled CMake to execute unit test cases
    Reworked CMake instructions in README

--- cmake/BuildTypes.cmake
+++ cmake/BuildTypes.cmake
@@ -7,13 +7,5 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
       STRING "Choose the type of build." FORCE)
   # Set the possible values of build type for cmake-gui
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
-    "Debug" "Release" "UnitTest")
-endif()
-
-if (CMAKE_BUILD_TYPE STREQUAL "UnitTest")
-  if(MSVC)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /Zi")
-  else()
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g")
-  endif()
+    "Debug" "Release" "RelWithDebInfo" "MinSizeRel")
 endif()
