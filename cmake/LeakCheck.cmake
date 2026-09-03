96fd3dba7a9e9a3e73760c5b3bc741b3d905c880
cogu <congus8@gmail.com>
2026-09-03 23:23:11 +0200

    Delete old cmake files

--- cmake/LeakCheck.cmake
+++
@@ -1,4 +0,0 @@
-option(LEAK_CHECK "Memory Leak Detection" OFF)
-if (NOT LEAK_CHECK AND CMAKE_BUILD_TYPE STREQUAL "UnitTest")    
-    set(LEAK_CHECK ON CACHE BOOL "Always on for UnitTest" FORCE)
-endif()
--- cmake/UnitTest.cmake
+++
@@ -1,4 +0,0 @@
-option(UNIT_TEST "Unit Test Build" OFF)
-if (NOT UNIT_TEST AND CMAKE_BUILD_TYPE STREQUAL "UnitTest")    
-    set(UNIT_TEST ON CACHE BOOL "Always on for UnitTest" FORCE)
-endif()
